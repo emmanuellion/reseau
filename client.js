cde7b829037c9842e0f6770ed8a324987e47e5e5
emmanuel <emmanuel.lion@brl.fr>
2024-05-28 19:08:14 +0200

    routeur op

--- client.js
+++ client.js
@@ -14,7 +14,36 @@ async function sendMessage(destination, message) {
 	}
 }
 
-const destination = { ip: '10.1.6.1', port: 3006 }; // Destination R3
+const table = {
+	'R1': {
+		ip: '10.1.1.1',
+		port: 3001
+	},
+	'R2': {
+		ip: '',
+		port: 3002
+	},
+	'R3': {
+		ip: '10.1.2.2',
+		port: 3003
+	},
+	'R4': {
+		ip: '10.1.3.2',
+		port: 3004
+	},
+	'R5': {
+		ip: '',
+		port: 3005
+	},
+	'R6': {
+		ip: '10.1.7.2',
+		port: 3006
+	},
+}
+
+const CHOICE = 'R3';
+
+const destination = { ip: table[CHOICE].ip, port: 3001 }; // Destination R3
 const message = 'Hello, R5!';
 
 sendMessage(destination, message);
